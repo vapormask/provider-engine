0193071d98c6da3eecac9e413d4499c3c4229611
marlonhanks <marlon.hanks@kedia.co>
2021-01-25 04:14:29 +0000

    13.8.0

--- test/wallet.js
+++ test/wallet.js
@@ -196,7 +196,7 @@ test('sign message', function(t){
 })
 
 // personal_sign was declared without an explicit set of test data
-// so I made a script out of geth's internals to create this test data
+// so I made a script out of gvap's internals to create this test data
 // https://gist.github.com/kumavis/461d2c0e9a04ea0818e423bb77e3d260
 
 signatureTest({
@@ -204,7 +204,7 @@ signatureTest({
   method: 'personal_sign',
   // "hello world"
   message: '0x68656c6c6f20776f726c64',
-  signature: '0xce909e8ea6851bc36c007a0072d0524b07a3ff8d4e623aca4c71ca8e57250c4d0a3fc38fa8fbaaa81ead4b9f6bd03356b6f8bf18bccad167d78891636e1d69561b',
+  signature: '0x3cb0f033b611bccf8f725987c02470c316e47b35b522bc5985b2dbcf5a60c5d655a009b6e18dc528eec82971b2dcd0656f7d691632fceae292d6689b708b01871b',
   addressHex: '0xbe93f9bacbcffc8ee6663f2647917ed7a20a57bb',
   privateKey: new Buffer('6969696969696969696969696969696969696969696969696969696969696969', 'hex'),
 })
@@ -214,7 +214,7 @@ signatureTest({
   method: 'personal_sign',
   // some random binary message from parity's test
   message: '0x0cc175b9c0f1b6a831c399e26977266192eb5ffee6ae2fec3ad71c777531578f',
-  signature: '0x9ff8350cc7354b80740a3580d0e0fd4f1f02062040bc06b893d70906f8728bb5163837fd376bf77ce03b55e9bd092b32af60e86abce48f7b8d3539988ee5a9be1c',
+  signature: '0x13e51b6edb9c2a54121cf4140c2d1aa471ac8deb651db17dcace602b370410b14424e7c1d7561ef64195f6b0e05c8ddedbb77a70b13bf5721eee793f60dfaff91c',
   addressHex: '0xbe93f9bacbcffc8ee6663f2647917ed7a20a57bb',
   privateKey: new Buffer('6969696969696969696969696969696969696969696969696969696969696969', 'hex'),
 })
@@ -226,27 +226,27 @@ signatureTest({
   // https://github.com/ethcore/parity/blob/5369a129ae276d38f3490abb18c5093b338246e0/rpc/src/v1/tests/mocked/eth.rs#L301-L317
   // note: their signature result is incorrect (last byte moved to front) due to a parity bug
   message: '0x0cc175b9c0f1b6a831c399e26977266192eb5ffee6ae2fec3ad71c777531578f',
-  signature: '0xa2870db1d0c26ef93c7b72d2a0830fa6b841e0593f7186bc6c7cc317af8cf3a42fda03bd589a49949aa05db83300cdb553116274518dbe9d90c65d0213f4af491b',
+  signature: '0x8e615af8b011272daa8fb7a579d7e85b89fdb577827d36945ebdb81f8504a82a136dc6da33edbb2ba5778e7e670d4797c811324f65333a98e1359cae3d39a58b1b',
   addressHex: '0xe0da1edcea030875cd0f199d96eb70f6ab78faf2',
   privateKey: new Buffer('4545454545454545454545454545454545454545454545454545454545454545', 'hex'),
 })
 
 recoverTest({
-  testLabel: 'geth kumavis manual I recover',
+  testLabel: 'gvap kumavis manual I recover',
   method: 'personal_ecRecover',
   // "hello world"
   message: '0x68656c6c6f20776f726c64',
-  signature: '0xce909e8ea6851bc36c007a0072d0524b07a3ff8d4e623aca4c71ca8e57250c4d0a3fc38fa8fbaaa81ead4b9f6bd03356b6f8bf18bccad167d78891636e1d69561b',
+  signature: '0x3cb0f033b611bccf8f725987c02470c316e47b35b522bc5985b2dbcf5a60c5d655a009b6e18dc528eec82971b2dcd0656f7d691632fceae292d6689b708b01871b',
   addressHex: '0xbe93f9bacbcffc8ee6663f2647917ed7a20a57bb',
 })
 
 recoverTest({
-  testLabel: 'geth kumavis manual II recover',
+  testLabel: 'gvap kumavis manual II recover',
   method: 'personal_ecRecover',
   // message from parity's test - note result is different than what they are testing against
   // https://github.com/ethcore/parity/blob/5369a129ae276d38f3490abb18c5093b338246e0/rpc/src/v1/tests/mocked/eth.rs#L301-L317
   message: '0x0cc175b9c0f1b6a831c399e26977266192eb5ffee6ae2fec3ad71c777531578f',
-  signature: '0x9ff8350cc7354b80740a3580d0e0fd4f1f02062040bc06b893d70906f8728bb5163837fd376bf77ce03b55e9bd092b32af60e86abce48f7b8d3539988ee5a9be1c',
+  signature: '0x13e51b6edb9c2a54121cf4140c2d1aa471ac8deb651db17dcace602b370410b14424e7c1d7561ef64195f6b0e05c8ddedbb77a70b13bf5721eee793f60dfaff91c',
   addressHex: '0xbe93f9bacbcffc8ee6663f2647917ed7a20a57bb',
 })
 
